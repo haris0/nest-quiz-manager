cade9191956fb830413eb21079c269f2bef82c59
Haris <hari.surrisyad@ruangguru.com>
2022-10-29 11:48:43 +0700

    using DTO and pipe validation

---
+++ src/modules/quiz/dto/CreateQuiz.dto.ts
@@ -0,0 +1,11 @@
+import { IsNotEmpty, Length } from 'class-validator';
+
+export class CreateQuizDto {
+  @IsNotEmpty({ message: 'Title is mandatory' })
+  @Length(3, 225)
+  title: string;
+
+  @IsNotEmpty()
+  @Length(3)
+  description: string;
+}
--- src/modules/quiz/quiz.controller.ts
+++ src/modules/quiz/quiz.controller.ts
@@ -1,9 +1,30 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CreateQuizDto } from './dto/CreateQuiz.dto';
+import { QuizService } from './quiz.service';
 
 @Controller('quiz')
 export class QuizController {
+  constructor(private quizService: QuizService) {}
+
   @Get('/')
   getAllQuiz() {
-    return [1, 2, 3];
+    return this.quizService.getAllQuiz();
+  }
+
+  @Post('/create')
+  @HttpCode(200)
+  @UsePipes(ValidationPipe)
+  createQuiz(@Body() quizData: CreateQuizDto) {
+    return {
+      data: quizData,
+    };
   }
 }
